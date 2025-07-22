0606701beb70748517186a8ac73ea1dddc50542a
davideaded <david.castrodc033@gmail.com>
2025-07-22 11:51:41 -0300

    api route

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import index from './index.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1', index);
+
+app.get('/{*any}', function (req, res, next) {
+    const error = new Error(
+        `${req.ip} tried to access ${req.originalUrl}`,
+    );
+    error.statusCode = 301;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    if (!error.statusCode) error.statusCode = 500;
+    if (error.statusCode === 301) {
+        return res.status(301).json('Not found');
+    }
+    return res
+        .status(error.statusCode)
+        .json({ error: error.toString() });
+});
+
+app.listen(3000, () => 'server listening...');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import { express } from 'express';
-const app = express();
+import express from 'express';
+const index = express.Router();
+
+index.route('/score')
+    .get((req, res) => {
+        res.status(200).json({ message: 'Hello, world!' });
+    })
+    .post((req, res) => {
+        res.status(201).json({ message: 'Score created!' });
+    });
+
+export default index;
