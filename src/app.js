3521c77846583b8faf01c4ae4f0a0498c641b2d0
davideaded <david.castrodc033@gmail.com>
2025-07-23 15:34:23 -0300

    cors issue

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import index from './index.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+    origin: 'http://localhost:8000'
+}));
 
 app.use('/api/v1', index);
 
