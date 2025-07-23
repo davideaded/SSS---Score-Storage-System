511d4f811352a049c59e6da6977bddc49ab04eb6
davideaded <david.castrodc033@gmail.com>
2025-07-23 16:44:30 -0300

    added origin for cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: 'http://localhost:8000'
+    origin: 'http://davidead.github.io/snakegame'
 }));
 
 app.use('/api/v1', index);
