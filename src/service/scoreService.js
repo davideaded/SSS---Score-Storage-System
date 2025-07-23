1ca1e2fcf4e1fa42bc50ddcac7356bac8e58d667
davideaded <david.castrodc033@gmail.com>
2025-07-23 12:49:16 -0300

    retrieve score by descending order

--- src/service/scoreService.js
+++ src/service/scoreService.js
@@ -4,7 +4,11 @@ import { NotFoundError } from '../errors/errors.js'
 const prisma = new PrismaClient({});
 
 export async function getAllScores() {
-    const scores = await prisma.score.findMany();
+    const scores = await prisma.score.findMany({
+        orderBy: {
+            value: 'desc'
+        }
+    });
     if (!scores[0]) throw new NotFoundError("Scores don't exist");
     return scores;
 }
