1f9c7a86fe8e19b3a9a25865bcd44373e9324c55
davideaded <david.castrodc033@gmail.com>
2025-07-24 08:22:58 -0300

    fix cors url
    fix cors url

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-    origin: 'http://davidead.github.io/snakegame'
-}));
+app.use(cors());
 
 app.use('/api/v1', index);
 
